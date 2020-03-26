ac93d238368d8e53dc954217dc91127c036f73be
OussamaMater <oussamamater@outlook.com>
2020-03-26 17:59:31 +0100

    Fixed a bug

--- www/app/Config.php
+++ www/app/Config.php
@@ -5,7 +5,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'PFA');
 //Defining the AppRoot directory
-define('APPROOT', dirname(__FILE__));
+define('APPROOT', realpath(dirname(__FILE__)));
 //Defining the URL of the app
 define('URLROOT', 'http://localhost/www');
 //Defining the SiteName
